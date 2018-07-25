0d266805b7659a0adbf438a8c5298adfe4fc0f09
swkane <samuelwkane@gmail.com>
2018-07-25 16:11:20 -0400

    employee form

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+class EmployeeList extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Employee List</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList;
